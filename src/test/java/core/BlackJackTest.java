b85084f73ef7b36ef836ab02833a3d4b47528bac
Noah Beeney <noahbeeney@hotmail.com>
2018-09-20 01:21:34 -0400

    test: added more tests for BlackJack class

--- src/test/java/core/BlackJackTest.java
+++ src/test/java/core/BlackJackTest.java
@@ -24,35 +24,48 @@ public class BlackJackTest extends TestCase{
 		game.show();
 	}
 	
+	public void testIsOver() {
+		BlackJack game = new BlackJack();
+		game.init();
+	
+		game.hit(game.getPlayer(), "C10");
+		game.hit(game.getPlayer(), "C10");
+		game.hit(game.getPlayer(), "C10");
+		assertEquals(true, game.isOver());
+	}
+	
 	public void testHit() {
 		BlackJack game = new BlackJack();
 		game.init();
 		int playerScore = game.playerScore();
 		game.show();
-
-		game.hit(game.getPlayer(), "D3");
-		playerScore += 3;
-		game.show();
-		assertEquals(playerScore, game.playerScore()); //Will fail occasionally because playerScore variable doesn't normalize aces
 		
-		game.hit(game.getPlayer(), "H5");
-		playerScore += 5;
+		game.hit(game.getPlayer(), "DA");
+		if(playerScore < 11) {
+			playerScore += 11;
+			
+		} else {
+			playerScore += 1;
+		}
 		game.show();
 		assertEquals(playerScore, game.playerScore());
 		
-		game.hit(game.getPlayer(), "C10");
-		playerScore += 10;
+		game.hit(game.getPlayer(), "HA");
+		if(playerScore < 11) {
+			playerScore += 11;
+			
+		} else {
+			playerScore += 1;
+		}
 		game.show();
-		assertEquals(true, game.isOver());
+		assertEquals(playerScore, game.playerScore());
 	}
 	
 	public void testStand() {
 		BlackJack game = new BlackJack();
 		game.init();
 		game.show();
-		
-		//assertEquals(true, game.isPlayersTurn()); <- can't guarantee this will be true; dealer could get dealt blackjack
-		
+				
 		while(game.playerScore() < 16) {
 			game.hit(game.getPlayer());
 		}
@@ -63,4 +76,5 @@ public class BlackJackTest extends TestCase{
 		game.show();
 		assertEquals(false, game.isPlayersTurn());
 	}
+
 }
